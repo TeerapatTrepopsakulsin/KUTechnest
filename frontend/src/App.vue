46f9fc8113047135165a59fe5f48793776656101
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2025-08-26 11:11:34 +0700

    Apply Tailwind to header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,37 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 </script>
 
 <template>
   <Header/>
-  <div class = main-container>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="mt-[70px]">
+    <!-- content -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
-    </div>
 </template>
 
 <style scoped>
-.main-container {
-  margin-top: 70px;
-}
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
