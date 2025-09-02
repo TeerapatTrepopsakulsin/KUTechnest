fd696e94e38123aff75b724bff8c6052741cf75a
Katharina Viik <katharinaviik@MacBook-Pro-Katharina.local>
2025-09-03 00:26:16 +0700

    Added response for CRUD and edited job post 12 per page

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet, PostViewSet, StudentRegisterView, StudentMeView,StudentViewSet
+from .views import CompanyViewSet, PostViewSet
+from django.contrib import admin
 
 router = DefaultRouter()
 router.register(r'companies', CompanyViewSet)
 router.register(r'posts', PostViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
+urlpatterns = router.urls
 
-    # Student endpoints
-    path('students/register/', StudentRegisterView.as_view(), name='student-register'),
-    path('students/me/', StudentMeView.as_view(), name='student-me'),
-]
